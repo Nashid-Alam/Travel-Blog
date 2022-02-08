564b0977fac3c6c5c46dbde4696f134bb022d18e
Nashid-Alam <nashid17@gmail.com>
2022-02-07 23:06:38 -0500

    added views and urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
 from . import views
 
+
 urlpatterns = [
-    # path('post/<int:pk>', views.post_detail, name='post_detail')
+    path('posts/', views.PostList.as_view(), name='post_list'),
+    path('post/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
+    path('discussion/', views.DiscussionList.as_view(), name='discussion_list'),
+    path('discussion/<int:pk>', views.DiscussionDetail.as_view(), name='discussion_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
-from .models import City, Discussion, Do, Picture, Place, Post
+from rest_framework import generics
+from .serializers import PostSerializer, DiscussionSerializer, DoSerializer, PictureSerializer, PlaceSerializer
+from . import models
 
+
+class PostList(generics.ListCreateAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class DiscussionList(generics.ListCreateAPIView):
+    queryset = models.Discussion.objects.all()
+    serializer_class = DiscussionSerializer
+
+
+class DiscussionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Discussion.objects.all()
+    serializer_class = DiscussionSerializer
+
+
+class DoList(generics.ListCreateAPIView):
+    queryset = models.Do.objects.all()
+    serializer_class = DoSerializer
+
+
+class DoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Do.objects.all()
+    serializer_class = DoSerializer
+
+
+class PlaceList(generics.ListCreateAPIView):
+    queryset = models.Place.objects.all()
+    serializer_class = PlaceSerializer
+
+
+class PlaceDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Place.objects.all()
+    serializer_class = PlaceSerializer
+
+
+class PictureList(generics.ListCreateAPIView):
+    queryset = models.Picture.objects.all()
+    serializer_class = PictureSerializer
+
+
+class PictureDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Picture.objects.all()
+    serializer_class = PictureSerializer
