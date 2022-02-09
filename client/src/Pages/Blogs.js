172dc5f153960223643eaac14d31b58abe6be89c
Nashid-Alam <nashid17@gmail.com>
2022-02-08 20:39:42 -0500

    got the page of blogs set up with axios call

--- client/src/Pages/Blogs.js
+++ client/src/Pages/Blogs.js
@@ -1,8 +1,40 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import BlogCard from '../components/BlogCard'
+
+const BASE_URL = process.env.REACT_APP_BASE_URL
 
 function Blogs(props) {
+  const [blogs, setBlogs] = useState([])
+  const [city, setCity] = useState({})
+  const city_id = props.match.params.city_id
 
-  return <div>blos</div>
+  const getCityInfo = async () => {
+    const response = await axios.get(`${BASE_URL}/blog-api/cities/${city_id}`)
+    setCity(response.data)
+    setBlogs(response.data.posts)
+  }
+
+  useEffect(() => {
+    getCityInfo()
+  }, [])
+
+  console.log(blogs)
+
+  return (
+    <div>
+      <h1>List of Blogs in {city.name}</h1>
+      {blogs.map((blog) => {
+        return (
+          <BlogCard
+            key={blog.id}
+            blog={blog}
+            // onClick={() => props.history.push(`/blogs/${city.id}`)}
+          />
+        )
+      })}
+    </div>
+  )
 }
 
 export default Blogs
--- client/src/Pages/Cities.js
+++ client/src/Pages/Cities.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react"
 import CityCard from "../components/CityCard"
 import axios from "axios"
+const BASE_URL = process.env.REACT_APP_BASE_URL
 
 function Cities(props) {
   const [cities, setCities] = useState([])
 
   const getAllCities = async () => {
-    const response = await axios.get("http://localhost:8000/blog-api/cities/")
+    const response = await axios.get(`${BASE_URL}/blog-api/cities/`)
     setCities(response.data)
   }
 
